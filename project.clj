be7da248690a986c763d40a8a1bebb96395ca57d
Shogo Ohta <athos0220@gmail.com>
2018-10-03 14:46:42 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aintegrant "0.6.3-SNAPSHOT"
   :description "Aintegrant ain't Integrant, it's Async Integrant!"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/athos/aintegrant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[integrant "0.6.3"]]
