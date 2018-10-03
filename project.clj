c2fd2744f9b7ecb59ffac686e744f91ed0f61631
Shogo Ohta <athos0220@gmail.com>
2018-10-03 14:42:03 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject aintegrant "0.6.3-SNAPSHOT"
+  :description "Aintegrant ain't Integrant, it's Async Integrant!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[integrant "0.6.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
---
+++ src/aintegrant/core.clj
@@ -0,0 +1,6 @@
+(ns aintegrant.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
