f0aa8e29898ef5150a98b58dd55ec7f9337a2910
Shogo Ohta <athos0220@gmail.com>
2018-10-04 14:47:20 +0900

    Implement simple CPS init/resume

--- src/aintegrant/core.clj
+++ src/aintegrant/core.clj
@@ -30,12 +30,57 @@
 (defmethod suspend-key! :default [k v callback]
   (halt-key! k v callback))
 
+(defn- exec-async [f]
+  (f))
+
+(defn- wrap-callback [callback]
+  (fn
+    ([ret] (callback nil ret))
+    ([err ret] (callback err ret))))
+
+(defn- try-build-action [system f k v callback]
+  (let [callback' (wrap-callback callback)]
+    (try
+      (f k v callback')
+      (catch Throwable t
+        (callback t nil)))))
+
+(defn- build-key [f assertf system [k v] callback]
+  (let [v' (#'ig/expand-key system v)]
+    (assertf system k v)
+    (let [callback' (fn [err ret]
+                      (if err
+                        (callback err nil)
+                        (as-> system system
+                          (assoc system k ret)
+                          (vary-meta system assoc-in [::ig/build k] v')
+                          (callback nil system))))]
+      (try-build-action system f k v' callback'))))
+
+(defn build
+  ([config keys f callback]
+   (build config keys f (fn [_ _ _]) callback))
+  ([config keys f assertf callback]
+   (letfn [(step [system [kv & kvs]]
+             (if kv
+               (exec-async
+                #(build-key f assertf system kv
+                            (fn [err system]
+                              (if err
+                                (callback err nil)
+                                (step system kvs)))))
+               (callback nil system)))]
+     (let [relevant-keys   (#'ig/dependent-keys config keys)]
+       (step (with-meta {} {::ig/origin config})
+             (map (fn [k] [k (config k)]) relevant-keys))
+       nil))))
+
 (defn init
   ([config callback]
    (init config (keys config) callback))
   ([config keys callback]
    {:pre [(map? config)]}
-   nil))
+   (build config keys init-key callback)))
 
 (defn halt!
   ([system callback]
@@ -49,7 +94,12 @@
    (resume config system keys callback))
   ([config system keys callback]
    {:pre [(map? config) (map? system) (some-> system meta ::ig/origin)]}
-   nil))
+   (build config keys
+          (fn [k v]
+            (if (contains? system k)
+              (resume-key k v (-> system meta ::build (get k)) (system k))
+              (init-key k v)))
+          callback)))
 
 (defn suspend!
   ([system callback]
