b89a52c477b53783046c721a01ef3410e022c07b
Shogo Ohta <athos0220@gmail.com>
2018-10-30 09:36:31 +0900

    Support Closure Promise as a async executor impl

--- src/aintegrant/async/promise.cljs
+++ src/aintegrant/async/promise.cljs
@@ -1,15 +1,32 @@
 (ns aintegrant.async.promise
-  (:require [aintegrant.async.core :as async]))
+  (:require [aintegrant.async.core :as async])
+  (:import goog.Promise))
+
+(defn native-promise-executor []
+  (reify async/AsyncExecutor
+    (-exec [this f]
+      (js/Promise. f))))
+
+(defn closure-promise-executor []
+  (reify async/AsyncExecutor
+    (-exec [this f]
+      (Promise. f))))
 
 (def promise-async-executor
-  (let [executor (when (exists? js/Promise)
-                   (reify async/AsyncExecutor
-                     (-exec [this f]
-                       (js/Promise. f))))]
+  (let [executor (cond (exists? js/Promise) (native-promise-executor)
+                       (or (exists? js/setImmediate)
+                           (exists? js/setTimeout))
+                       (closure-promise-executor)
+                       :else nil)]
     (fn [] executor)))
 
+(extend-type Promise
+  async/AsyncTask
+  (-then [this resolve reject]
+    (.then this resolve reject)))
+
 (when (exists? js/Promise)
-  (extend-protocol async/AsyncTask
-    js/Promise
+  (extend-type js/Promise
+    async/AsyncTask
     (-then [this resolve reject]
       (.then this resolve reject))))
